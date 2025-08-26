e7d457cc7b4fc1e4b7b1ebc987511b60da4d4044
Moshe-Feld <moshfeld@gmail.com>
2025-08-26 21:47:25 +0300

    get random quote

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import axios from 'axios'
 function App() {
-  const [count, setCount] = useState(0)
+  const [Quote, setQuote] = useState("");
+  const [load, setLoad] = useState(true);
 
+  useEffect(() => {
+    let ignore = false;
+    const fetchQuote = async () => {
+      const randomData = await axios.get('https://dummyjson.com/quotes/random');
+      if (!ignore) {
+        console.log("useEffect runing")
+        setQuote(randomData.data);
+      }
+      setLoad(false);
+    }
+    fetchQuote();
+    return () => {
+      ignore = true;
+    }
+
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello World</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {load ? <p>loading</p> : <p><strong>my random quote:</strong> {Quote.quote}</p>}
     </>
   )
 }
